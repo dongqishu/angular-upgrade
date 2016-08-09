b115cf50692fd5e48aa4e30d24ec6d5f89db8850
Ryan Langton <rlangton76@gmail.com>
2016-08-08 19:38:36 -0500

    fix product component

--- Phase5/src/reviews/reviews.component.ts
+++ Phase5/src/reviews/reviews.component.ts
@@ -1,3 +1,38 @@
+class ReviewsController{
+    hoveringOver: number;
+    reviews: ProductReview;
+
+    $onInit(){
+        this.hoveringOver = 0;
+    }
+    
+    starHover(starNum: number): void {
+        console.log("star hover " + starNum);
+        this.hoveringOver = starNum;
+    }
+
+    starStyle(starNum: number): any {
+        if (starNum <= this.hoveringOver || (starNum <= this.reviews.myRating && this.hoveringOver === 0)) {
+            return { "color" : "gold" };
+        }
+        return {};
+    }
+
+    hasStar(starNum: number): boolean {
+        if ((starNum <= this.reviews.stars && this.reviews.myRating === null) 
+        || starNum <= this.hoveringOver || 
+        (starNum <= this.reviews.myRating && this.hoveringOver === 0)){
+            return true;
+        }
+        return false;
+    }
+
+    starExit(): void {
+        console.log("star exit");
+        this.hoveringOver = 0;
+    }
+}
+
 angular
     .module('app')
     .component('appReviews', {
@@ -8,38 +43,3 @@ angular
             onRate: "&"
         }    
     });
-
-class ReviewsController{
-    private hoveringOver: number;
-    private reviews: any;
-
-    private $onInit(){
-        this.hoveringOver = 0;
-    }
-    
-    private starHover(starNum: number): void {
-        console.log("star hover " + starNum);
-        this.hoveringOver = starNum;
-    }
-
-    private starStyle(starNum: number): any {
-        if (starNum <= this.hoveringOver || (starNum <= this.reviews.myRating && this.hoveringOver === 0)) {
-            return { "color" : "gold" };
-        }
-        return {};
-    }
-
-    private hasStar(starNum: number): boolean{
-        if ((starNum <= this.reviews.stars && this.reviews.myRating === null) 
-        || starNum <= this.hoveringOver || 
-        (starNum <= this.reviews.myRating && this.hoveringOver === 0)){
-            return true;
-        }
-        return false;
-    }
-
-    private starExit(): void{
-        console.log("star exit");
-        this.hoveringOver = 0;
-    }
-}
